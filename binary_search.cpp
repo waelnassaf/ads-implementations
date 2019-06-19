07af583615d6502d704474dd5ed10f8e3f8d17a9
Wael <waelassaf121@gmail.com>
2019-06-19 14:50:29 +0300

    Compute array size dynamically

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int binary_search(int a[], int x, int size) {
+int binary_search(int a[], int size, int x) {
     int low  = 0;
     int high = size - 1;
     int mid;
-    
+
     while (low <= high) {
         mid = (low + high) / 2;
         if (a[mid] < x)
             low = mid + 1;
         else if (a[mid] > x)
-            high  = mid - 1; 
-        else 
+            high  = mid - 1;
+        else
             return mid;
     }
     return -1;
@@ -21,11 +21,10 @@ int binary_search(int a[], int x, int size) {
 
 int main()
 {
-    
+
     int arr[] = {12, 32, 39, 88, 120, 1211};
-    
-    cout << binary_search(arr, 120, 6) << endl;
-    
+    int s = sizeof(arr)/sizeof(arr[0]);
+    cout << binary_search(arr, s, 39) << endl;
+
     return 0;
 }
-

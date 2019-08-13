829968328df2e587ab728a80ab41e8b67ded9ac6
Wael <waelassaf121@gmail.com>
2019-08-13 08:32:43 +0300

    Reduce Clutter + DFS

--- New Lab/binary_search_tree.cpp
+++ New Lab/binary_search_tree.cpp
@@ -1,54 +1,47 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+
 class Node
 {
 public:
     int data;
     Node* left;
     Node* right;
-    Node() {
-        left = right = 0;
-        data = -1;
-    }
-    Node(int data){
+    Node () {   left = right = 0; data = -1;   }
+    Node (int data){
         left = right = 0;
         this->data = data;
     }
 };
 
-class BST
-{
-private:
-    Node* root;
+class BST {
 public:
-    BST()   {   root = 0;  }
+    Node* root;
+    BST() {   root = 0;  }
 
     void InsertNode(int data)
     {
-
         Node* newNode = new Node(data);
-        if(root == 0){
-            root= newNode;
-            return;
+        if(root == 0) {
+            root = newNode;
+        } else {
+            Node* prev = 0;
+            Node*  temp = root;
+            while (temp != 0) {
+                prev = temp;
+                if (data < temp->data) temp = temp->left;
+                else temp = temp->right;
+            }
+            if (data > prev->data) prev->right = newNode;
+            else prev->left = newNode;
         }
-        Node* prev = 0;
-        Node*  temp = root;
-        while (temp != 0)
-        {
-            prev =  temp;
-            if (data < temp->data)
-                temp = temp->left;
-            else
-                temp= temp->right;
-        }
-        if(data > prev->data) prev->right = newNode;
-        else prev->left = newNode;
     }
-    int Search (int data,int &count)
+
+    int Search(int data, int &count)
     {
-        int lvl = 0;
-        count   = 1;
+        int lvl = 0;    // Tree Level
+        count   = 1;    // # Comparisons
         if (root->data == data) return 0;
 
         Node* temp = root;
@@ -61,16 +54,18 @@ public:
             }
             else if (data > temp->data)
             {
-                temp= temp->right;
+                temp = temp->right;
                 lvl++;
             }
-            else{
+            else {
                 return lvl;
             }
             count++;
         }
+        return -1;
     }
 
+    // Traverse the tree with a breadth first algorithm.
     void bfs() {
         queue<Node*> q;
         q.push(root);
@@ -83,6 +78,30 @@ public:
         }
     }
 
+    // Depth first 1
+    void InOrder(Node* temp) {
+        if (temp) {
+            InOrder(temp->left);
+            cout << temp->data << " ";
+            InOrder(temp->right);
+        }
+    }
+    // Depth first 2
+    void PreOrder(Node* temp) {
+        if (temp) {
+            cout << temp->data << " ";
+            PreOrder(temp->left);
+            PreOrder(temp->right);
+        }
+    }
+    // Depth first 3
+    void PostOrder(Node* temp) {
+        if (temp) {
+            PostOrder(temp->left);
+            PostOrder(temp->right);
+            cout << temp->data << " ";
+        }
+    }
 
 };
 
@@ -93,6 +112,7 @@ int main()
     t.InsertNode(30);
     t.InsertNode(10);
     t.InsertNode(13);
-    t.bfs();
+//    t.bfs();
+    t.PostOrder(t.root);
     return 0;
 }

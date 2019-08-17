604e36c36b3b13c4c6395ac096fccc5c3743817a
Wael <waelassaf121@gmail.com>
2019-08-18 02:31:17 +0300

    Reduce Clutter

--- Theoretical Code/kth_largest_element.cpp
+++ Theoretical Code/kth_largest_element.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 int main()
 {
     int n, k;
-    cout << "Enter the size of the array: n" << endl;
+    cout << "Enter the size of the array: ";
     cin >> n;
     int arr[n];
     cout << "Enter the elements of the array" << endl;
-    
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
-    }
-    
+
     sort(arr, arr+n, greater<int>()); // To sort the array descending.
-    
+
     cout << "Enter your Kth largest element?" << endl;
     cin >> k;
-    
+
     cout << "The " << k << " largest element is: " << endl;
-    cout << arr[k-1] << endl;    
-    
+    cout << arr[k-1] << endl;
+
     return 0;
 }
-
